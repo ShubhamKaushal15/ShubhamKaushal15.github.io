4daadd73b52270400ba6bd251cf18234a2cf9c65
ShubhamKaushal15 <shubham.a.kaushal@gmail.com>
2023-07-22 10:05:39 -0400

    changed time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("August 15, 2023 09:30:00 EST").getTime();
+var countDownDate = new Date("August 15, 2023 8:30:00 EST").getTime();
 
 // Update the countdown every 1 second
 var countdownTimer = setInterval(function() {
